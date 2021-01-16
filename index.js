854d9e5422fdb392a8b830ae4e1f1b2616d761b0
unknown <>
2021-01-16 18:48:23 +0300

    v 0.1

--- index.js
+++ index.js
@@ -59,10 +59,12 @@ app.post("/sil", function(req, res){
         res.redirect("/");
     })
 });
+
+
 let port = process.env.PORT;
 if(port == "" || port == null){
   port = 5000;
 }
 app.listen(port, function(){
-  console.log("port : " + port);
+  console.log("port numarasi : " + port);
 });
